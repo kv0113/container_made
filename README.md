e882bc7d4ab7741678d24263bf8f5c405bf8832d
kv0113 <kbcaclg@gmail.com>
2023-07-28 18:58:51 +0530

    0.0.2

--- README.md
+++ README.md
@@ -11,29 +11,71 @@ and the Flutter guide for
 [developing packages and plugins](https://flutter.dev/developing-packages).
 -->
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+description : it provide already made container where you can add two color as gradient color, title, subtitle. 
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+in project there is customs made container where you just have to add same details.dart pub publish
+
+# already made  Containers
+
+custom container package lets you add a beautiful gradient container to your Flutter app.
+
+## Installation
+
+1. Add the latest version of package to your pubspec.yaml (and run`dart pub get`):
+```yaml
+dependencies:
+  container_made : ^0.0.1
+```
+2. Import the package and use it in your Flutter App.
+```dart
+import 'package: containers_made/containers_made.dart';
+```
+## Example
+There are a number of properties that you can modify:
+
+-  height
+- width
+- title
+- subtitle
+- gradient (color1 and color2)
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+start when ever you want to use custom container
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
+there is already example in there
 
 ```dart
-const like = 'sample';
+
+class uniqueContainer extends StatelessWidget {
+  const uniqueContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: ContainerMade(
+            title: 'Hello World',
+            subtitle: "This is new Package",
+            color1: Colors.white,
+            color2: Colors.black,
+          ),
+        ));
+  }
+}
 ```
 
-## Additional information
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+## Next Goals
+
+- [x] Add onTap for functions.
+  Now, you can specify the onTap and specify a function.
+
+- [x] Change font and color style for text.
+  Change color by specifying `textcolor` and `subtitlecolor` properties.
+
+- [ ] Add more containers to the package.
--- lib/src/container_made.dart
+++ lib/src/container_made.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class ContainerMade extends StatefulWidget {
   final double? width;
-  final double height;
-  final Color? color1;
-  final Color? color2;
+  final double? height;
+  final Color color1;
+  final Color color2;
   final String title;
   final Color? textColor;
-  final String? subtitle;
+  final String subtitle;
   final Color? subtitleColor;
 
   final EdgeInsetsGeometry? padding;
@@ -17,12 +17,12 @@ class ContainerMade extends StatefulWidget {
   const ContainerMade(
       {Key? key,
       this.width,
-      required this.height,
-      this.color1,
-      this.color2,
+      this.height,
+      required this.color1,
+      required this.color2,
       required this.title,
       this.textColor,
-      this.subtitle,
+      required this.subtitle,
       this.subtitleColor,
       this.padding,
       this.titleStyle,
@@ -36,57 +36,55 @@ class ContainerMade extends StatefulWidget {
 class _ContainerMadeState extends State<ContainerMade> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onTap ?? () {},
-      child: Container(
-        width: widget.width ?? MediaQuery.of(context).size.width * 0.90,
-        height: widget.height,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-          gradient: LinearGradient(colors: [
-            widget.color1 ?? const Color(0xFFCB1841),
-            widget.color2 ?? const Color(0xFF2604DE)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              blurRadius: 12,
-              offset: Offset(0, 6),
+    return Container(
+      width: widget.width ?? MediaQuery.of(context).size.width * 0.90,
+      height: widget.height,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+        gradient: LinearGradient(colors: [
+          widget.color1 ?? const Color(0xFFCB1841),
+          widget.color2 ?? const Color(0xFF2604DE)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 12,
+            offset: Offset(0, 6),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              widget.title,
+              style: widget.titleStyle ??
+                  TextStyle(
+                    color: widget.textColor,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                widget.title,
-                style: widget.titleStyle ??
-                    TextStyle(
-                      color: widget.textColor,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-            ),
-            widget.subtitle != null
-                ? Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text(
-                      widget.subtitle ?? "",
-                      style: widget.subtitleStyle ??
-                          TextStyle(
-                            color: widget.subtitleColor,
-                            fontSize: 15.0,
-                          ),
-                    ),
-                  )
-                : Container(),
-          ],
-        ),
+          ),
+          widget.subtitle != null
+              ? Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    widget.subtitle ?? "",
+                    style: widget.subtitleStyle ??
+                        TextStyle(
+                          color: widget.subtitleColor,
+                          fontSize: 15.0,
+                        ),
+                  ),
+                )
+              : Container(),
+        ],
       ),
     );
   }
 }
+
